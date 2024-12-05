77f9f992e930f6b5e1deadd3e051ead57a4bc092
Jammbeast <jammbeast69@mail.ru>
2024-12-06 01:48:55 +0300

    класс Dish

--- KuhnyaSerialSts/Models/Dish.cs
+++ KuhnyaSerialSts/Models/Dish.cs
@@ -38,4 +38,40 @@ namespace KuhnyaSerialSts.Models{
             this.types = types;
 
     }
+
+        public void EditDish(in int id, in string name, in string description, in string weight, in double price, in int timeToCook, params string[] types, in Category category){
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.weight = weight;
+            this.price = price;
+            this.timeToCook = timeToCook;
+            this.types = types;
+            this.category = category;
+            this.types = types;
+        }
+
+        public void DisplayDish(){
+            Console.WriteLine($"ID: {id}");
+            Console.WriteLine($"Название: {name}");
+            Console.WriteLine($"Описание: {description}");
+            Console.WriteLine($"Вес: {weight}");
+            Console.WriteLine($"Цена: {price}");
+             Console.WriteLine($"Категория: {category}");
+            Console.WriteLine($"Время приготовления: {timeToCook}");
+            Console.WriteLine($"Типы: {string.Join(", ", types)}");
+           
+        }
+
+        public void DeleteDish(){
+            id = 0;
+            name = "";
+            description = "";
+            weight = "";
+            price = 0;
+            timeToCook = 0;
+            types = null;
+            category = 0;
+        }
+    }
 }
