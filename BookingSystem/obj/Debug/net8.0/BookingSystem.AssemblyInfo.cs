7c7d54ea7b44e8f4db25cde5b30d281a773c1d53
Renate Zaripov <jammbeast69@mail.ru>
2024-12-12 20:27:50 +0000

    Initialized new console app for Lab1

--- BookingSystem/obj/Debug/net8.0/BookingSystem.AssemblyInfo.cs
+++ BookingSystem/obj/Debug/net8.0/BookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2cbce59d5a6cd9ad0f31d1d878ac8d1d78fc93a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce25a8c8c486b2ef19bd85e921913c2a1be284ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
