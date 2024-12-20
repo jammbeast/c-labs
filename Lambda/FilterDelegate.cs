c1dc2943928c9bc0618512ae31cf29754348c277
jammbeast <jammbeast69@mail.ru>
2024-12-20 10:49:37 +0300

    небольшая документация доделегировал полномочия Холодно тепло

---
+++ Lambda/FilterDelegate.cs
@@ -0,0 +1,1 @@
+public delegate bool FilterDelegate(string str); 
---
+++ Termo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+    {
+static void Main(string[] args)
+        {
+                Console.Write("мин темп");
+
+            double minTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("макс темп: ");
+            double maxTemp = Convert.ToDouble(Console.ReadLine());
+
+            Thermostat thermostat = new Thermostat(minTemp, maxTemp);
+
+            thermostat.OverHeat += message => Console.WriteLine(message);
+            thermostat.OverCool += message => Console.WriteLine(message);
+
+            thermostat.Start();
+
+        }
+    }
