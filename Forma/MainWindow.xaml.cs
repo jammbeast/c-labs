cabef3bf692b3bf3b0c065b981782622c98e6ad3
jammbeast <jammbeast69@mail.ru>
2024-12-20 11:42:19 +0300

    WPF

---
+++ Forma/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace RegistrationForm
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик нажатия на ссылки в TextBlock
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
+            e.Handled = true;
+        }
+
+        // Обработчик нажатия кнопки "Register"
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Registration successful!");
+        }
+    }
+}
