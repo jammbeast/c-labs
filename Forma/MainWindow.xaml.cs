59d84c8d6b0fc58f6effdbfff4fb774434b2d71c
jammbeast <jammbeast69@mail.ru>
2024-12-20 13:07:03 +0300

    проверка пароля

--- Forma/MainWindow.xaml.cs
+++ Forma/MainWindow.xaml.cs
@@ -13,16 +13,31 @@ namespace RegistrationForm
         }
 
         // Обработчик нажатия на ссылки в TextBlock
-        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+            private void RegisterButton_Click(object sender, RoutedEventArgs e)
+    {
+        var passwordBox = (PasswordBox)FindName("PasswordBox");
+        var confirmPasswordBox = (PasswordBox)FindName("ConfirmPasswordBox");
+
+        if (passwordBox.Password.Length <= 6)
+        {
+            MessageBox.Show("Password must be longer than 6 characters.");
+            return;
+        }
+
+        if (passwordBox.Password != confirmPasswordBox.Password)
+        {
+            MessageBox.Show("Passwords do not match.");
+            return;
+        }
+
+        MessageBox.Show("Registration successful!");
+    }
+     private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri) { UseShellExecute = true });
             e.Handled = true;
         }
 
-        // Обработчик нажатия кнопки "Register"
-        private void RegisterButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Registration successful!");
-        }
+        // Обработчик нажатия кнопки "Register
     }
 }
